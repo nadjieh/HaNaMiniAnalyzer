f8c6a6f91770b2051968abbce01e04a3f42e3b2d
nadjieh <ajafari@cern.ch>
2018-03-29 11:50:51 +0200

    latest combine results: July and Aug are from 2017

--- macro/combine/IntermediateProvider.C
+++ macro/combine/IntermediateProvider.C
@@ -10,25 +10,59 @@ using namespace std;
     fixes["n"] = 3.22;
     fixes["frac"] = 0.607;
 */
+
+/*** WITH PUBLISHED RESULTS (https://github.com/nadjieh/HaNaMiniAnalyzer/blob/80X/macro/combine/IntermediateProvider.C)
+ * meanerrors["a1"] = make_pair(0.0091, make_pair(0.00085,0.00085));
+ * meanerrors["b1"] = make_pair(0.0172, make_pair(0.0017, 0.0017));
+ * meanerrors["m1"] = make_pair(-0.069, make_pair(0.0066,0.0066));
+ * FixedParams fixes;
+ * fixes["alpha"] = 1.15;
+ * fixes["width"] = 0.008;
+ * fixes["n"] = 3.02;
+ * fixes["frac"] = 0.57;
+ */
+
 int main(int argc, char** argv) {
     double sigNormUnc = 0;
-    bool blind = true;
+	double sigEff = 1.0;
+	double bkgEff = 1.0;
     bool WideRange = false;
+    TString btag = "";
+    TString inFile = "DoubleMu2012_final_6_6.root";
+	TString cat = "";
     for (int f = 1; f < argc; f++) {
         std::string arg_fth(*(argv + f));
 	cout<<arg_fth<<endl;
         if (arg_fth == "sigNormUnc") {
-            f++;
-            std::string out(*(argv + f));
-            sigNormUnc = std::atof(out.c_str());
-        } else if (arg_fth == "unblind") {
-            //f++;
-	    blind = false;
-	} else if (arg_fth == "wide") {
-	    cout<<"WIDE"<<endl;
+	  f++;
+	  std::string out(*(argv + f));
+	  sigNormUnc = std::atof(out.c_str());
+        } else if (arg_fth == "wide") {
+	  cout<<"WIDE"<<endl;
             //f++;
             WideRange = true;
-        }
+        } else if (arg_fth == "btag") {
+	  f++;
+	  std::string out(*(argv + f));
+          btag = out.c_str();
+	} else if (arg_fth == "input") {
+	  f++;
+	  std::string out(*(argv + f));
+          inFile = out.c_str();
+	} else if (arg_fth == "cat") {
+	  f++;
+	  std::string out(*(argv + f));
+          cat = out.c_str();
+	} else if (arg_fth == "sigEff") {
+	  f++;
+	  std::string out(*(argv + f));
+	  sigEff = std::atof(out.c_str());
+    } else if (arg_fth == "bkgEff") {
+	  f++;
+	  std::string out(*(argv + f));
+	  bkgEff = std::atof(out.c_str());
+    }
+
     }
     MeanErr meanerrors;
     meanerrors["a1"] = make_pair(0.000, make_pair(0.0004,0.0004));
@@ -39,13 +73,24 @@ int main(int argc, char** argv) {
     fixes["width"] = 0.42;
     fixes["n"] = 2.7;
     fixes["frac"] = 0.121;
-    //for (double i = 25.; i < 65; i += 0.5) {
-    //for (double i = 30.; i < 30.01; i += 0.5) {
-    //	double i = 28;
+    /*meanerrors["a1"] = make_pair(0.0091, make_pair(0.00085,0.00085));
+    meanerrors["b1"] = make_pair(0.0172, make_pair(0.0017, 0.0017));
+    meanerrors["m1"] = make_pair(-0.069, make_pair(0.0066,0.0066));
+    FixedParams fixes;
+    fixes["alpha"] = 1.15;
+    fixes["width"] = 0.008;
+    fixes["n"] = 3.02;
+    fixes["frac"] = 0.57;*/
+
+
+    
     if(WideRange)
         cout<<"In provider :-)"<<endl;
-    WSProvider * myprovider = new WSProvider(meanerrors, fixes, sigNormUnc,WideRange);
-    myprovider->WriteWS(blind);
+    WSProvider * myprovider = new WSProvider(meanerrors, fixes, sigNormUnc,WideRange,btag);
+	myprovider->SetCatName(cat);
+	myprovider->SetSigEff(sigEff);
+	myprovider->SetBkgEff(bkgEff);
+    myprovider->WriteWS(inFile);
     delete myprovider;
     return 0;
 }
